866422076d389a1657d7ac2deffd87d29b37519c
tom1dev <tdev429@aucklanduni.ac.nz>
2023-07-08 23:30:40 +1200

    added timer to main activity

--- app/src/main/java/com/example/hackathonapp2/MainActivity.java
+++ app/src/main/java/com/example/hackathonapp2/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -13,8 +15,8 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     public static final int REQUEST_CODE_ADD_NOTE = 1;
     private ArrayList<String> stats = new ArrayList<String>();
-    private String distance ="1";
-    private String area="2";
+    private String distance ="1M";
+    private String area="2M^2";
     private String score="3";
 
     @Override
@@ -23,8 +25,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ImageView startButton = findViewById(R.id.StartTrail);
         ImageView stopButton = findViewById(R.id.StopTrail);
-        stopButton .setVisibility(View.GONE);
-        startButton .setVisibility(View.VISIBLE);
+        TextView counter = findViewById(R.id.timer);
+        stopButton.setVisibility(View.GONE);
+        startButton.setVisibility(View.VISIBLE);
 
         //start button press
         startButton.setOnClickListener(new View.OnClickListener() {
@@ -34,6 +37,32 @@ public class MainActivity extends AppCompatActivity {
 
                 stopButton.setVisibility(View.VISIBLE);
                 startButton.setVisibility(View.GONE);
+                new CountDownTimer(3600000, 1000) {
+
+
+                    public void onTick(long millisUntilFinished) {
+                        long seconds = millisUntilFinished / 1000;
+                        int minutes = Math.round(seconds/60);
+                        seconds = seconds - minutes*60;
+                        counter.setText(Integer.toString(minutes)+":"+Long.toString(seconds));
+                    }
+
+                    public void onFinish() {
+                        stats.add(score);
+                        stats.add(distance);
+                        stats.add(area);
+
+                        stopButton.setVisibility(View.GONE);
+                        startButton.setVisibility(View.VISIBLE);
+                        Intent i = new Intent(getApplicationContext(), StatisiticsActivitity.class);
+
+                        i.putExtra("key", stats);
+                        startActivity(i);
+
+                    }
+
+
+                }.start();
             }
 
         });
@@ -45,16 +74,15 @@ public class MainActivity extends AppCompatActivity {
                 stats.add(distance);
                 stats.add(area);
 
-                stopButton .setVisibility(View.GONE);
+                stopButton.setVisibility(View.GONE);
                 startButton.setVisibility(View.VISIBLE);
                 Intent i = new Intent(getApplicationContext(), StatisiticsActivitity.class);
 
-                i.putExtra("key",stats);
+                i.putExtra("key", stats);
                 startActivity(i);
-                }
+            }
 
         });
-
 
     }
 }
