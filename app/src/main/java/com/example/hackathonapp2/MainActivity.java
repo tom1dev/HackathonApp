1a9a92ad3ce015fc7476e7c98a3ee91782bb4f84
tom1dev <tdev429@aucklanduni.ac.nz>
2023-07-08 23:54:10 +1200

    added some comments

--- app/src/main/java/com/example/hackathonapp2/MainActivity.java
+++ app/src/main/java/com/example/hackathonapp2/MainActivity.java
@@ -18,11 +18,13 @@ import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
-    public static final int REQUEST_CODE_ADD_NOTE = 1;
+    //variables for stats
     private ArrayList<String> stats = new ArrayList<String>();
     private String distance ="1M";
     private String area="2M^2";
     private String score="3";
+
+    //variable for googlemap
 
     private GoogleMap myMap;
 
@@ -30,15 +32,23 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //variables for stop/start/timer
         ImageView startButton = findViewById(R.id.StartTrail);
         ImageView stopButton = findViewById(R.id.StopTrail);
+        TextView counter = findViewById(R.id.timer);
+        ImageView counterbox = findViewById(R.id.TimerBox);
 
+        //sets initial visibility for objects
+        counterbox.setVisibility(View.GONE);
         stopButton .setVisibility(View.GONE);
         startButton .setVisibility(View.VISIBLE);
+        counter.setVisibility(View.GONE);
+
+        //initialises google map
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        TextView counter = findViewById(R.id.timer);
+
 
 
         //start button press
@@ -46,9 +56,14 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
             @Override
             public void onClick(View v) {
+                //changes to correct visablity
+                counter.setVisibility(View.VISIBLE);
+                counterbox.setVisibility(View.VISIBLE);
 
                 stopButton.setVisibility(View.VISIBLE);
                 startButton.setVisibility(View.GONE);
+
+                //count down timer
                 new CountDownTimer(3600000, 1000) {
 
 
@@ -59,6 +74,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                         counter.setText(Integer.toString(minutes)+":"+Long.toString(seconds));
                     }
 
+                    //if timer reaches 0 move to stats page
                     public void onFinish() {
                         stats.add(score);
                         stats.add(distance);
@@ -80,6 +96,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         });
         //stop Button Press
         stopButton.setOnClickListener(new View.OnClickListener() {
+
+            //moves to stats page
             @Override
             public void onClick(View v) {
                 stats.add(score);
