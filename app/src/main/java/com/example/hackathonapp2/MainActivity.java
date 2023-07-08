5139d88806c84171c38435caea1b470d04929b7d
Tom Devonshire <tdev429@aucklanduni.ac.nz>
2023-07-08 14:07:55 +1200

    added buttons

--- app/src/main/java/com/example/hackathonapp2/MainActivity.java
+++ app/src/main/java/com/example/hackathonapp2/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.hackathonapp2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView startButton = findViewById(R.id.StartTrail);
+        ImageView stopButton = findViewById(R.id.StopTrail);
+        stopButton .setVisibility(View.GONE);
+
+        //start button press
+        startButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                stopButton.setVisibility(View.VISIBLE);
+                startButton.setVisibility(View.GONE);
+            }
+
+        });
+        //stop Button Press
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopButton .setVisibility(View.GONE);
+                startButton.setVisibility(View.VISIBLE);
+
+            }
+
+        });
+
+
     }
 }
