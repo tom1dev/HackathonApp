5014a99d9d3d297ef2704e360e273a787e6b444f
arlene <asap990@aucklanduni.ac.nz>
2023-07-08 23:01:39 +1200

    Added tutorial button and modified layout

---
+++ app/src/main/java/com/example/hackathonapp2/TutorialActivity.java
@@ -0,0 +1,27 @@
+package com.example.hackathonapp2;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class TutorialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tutorial);
+        Button btn = (Button)findViewById(R.id.supabutton);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(TutorialActivity.this, MainActivity.class));
+            }
+        });
+    }
+
+}
